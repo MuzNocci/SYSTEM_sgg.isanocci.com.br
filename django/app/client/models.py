7ca72b7c7a9904555012dde1b67fce8813d73927
Müller Nocciolli Ribeiro <muller.nocciolli@gmail.com>
2025-01-14 15:17:35 -0300

    Feat: Criação da tabela "Client", no banco de dados.

--- django/app/client/models.py
+++ django/app/client/models.py
@@ -1,3 +1,66 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+
+class Client(models.Model):
+
+
+    GENDER = [
+        ('N', 'Não informado'),
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+    ]
+
+    STATES = [
+        ('NI', 'Não informado'),
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranhão'),
+        ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'),
+        ('SE', 'Sergipe'),
+        ('TO', 'Tocantins'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    photo = models.ImageField(upload_to='app/images/clients/photos/', null=True, blank=True)
+    name = models.CharField(max_length=255)
+    instagram = models.CharField(max_length=30, null=True, blank=True)
+    email = models.EmailField(max_length=255)
+    smartphone = models.CharField(max_length=15)
+    whatsapp = models.BooleanField(default=False)
+    gender = models.CharField(max_length=1, choices=GENDER, default='N')
+    birth = models.DateField(null=True, blank=True)
+    cpf = models.CharField(max_length=14, null=True, blank=True)
+    rg = models.CharField(max_length=12, null=True, blank=True)
+    cep = models.CharField(max_length=8, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    address_number = models.CharField(max_length=6, null=True, blank=True)
+    complement = models.CharField(max_length=255, null=True, blank=True)
+    neighborhood = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    state = models.CharField(max_length=2, choices=STATES, default='NI')
+    observation = models.TextField()
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
