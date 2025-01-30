ad8ae82fafc607566b8a068a234e88b2260508ab
Müller Nocciolli Ribeiro <muller.nocciolli@gmail.com>
2025-01-30 19:57:16 -0300

    Feat: Criação do APP "Dashboard".

---
+++ django/app/dashboard/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from app.dashboard.views import DashboardView
+
+
+
+urlpatterns = [
+    
+    path('dashboard/', DashboardView.as_view(), name='dashboard_show'),
+
+]
--- django/app/dashboard/views.py
+++ django/app/dashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+
+class DashboardView(View):
+
+    def get(self, request):
+
+        context = {
+        }
+        return render(request, 'dashboard_show.html', context)
