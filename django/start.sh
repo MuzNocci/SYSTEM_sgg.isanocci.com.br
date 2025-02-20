781cf9af2ae6728833de746d77ff6894d267b635
Müller Nocciolli Ribeiro <muller.nocciolli@gmail.com>
2025-02-20 18:56:17 -0300

    Fix: Inclusão do Makemigrations e do Migrate, de forma geral.

--- django/start.sh
+++ django/start.sh
@@ -11,6 +11,9 @@ python manage.py migrate package
 python manage.py makemigrations gallery
 python manage.py migrate gallery
 
+python manage.py makemigrations
+python manage.py migrate
+
 python manage.py collectstatic --noinput
 
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 5
